85545ed7227c19d873b1c8841bd253edee4160f5
Ashirog1 <nguyennhatquoc1012@gmail.com>
2023-12-22 11:25:40 +0700

    add heuristics func

---
+++ src/heuristics.cpp
@@ -0,0 +1,45 @@
+//
+// Created by only_u on 12/21/23.
+//
+
+#include <complex>
+#include "heuristics.h"
+
+int HammingDistance(int size, std::vector<int> board) {
+    int hammingDistance = 0;
+    for (int i = 0; i < size * size; ++i) {
+        if (board[i] != i + 1 and board[i] != 0) {
+            ++hammingDistance;
+        }
+    }
+    return hammingDistance;
+}
+double CartesianDistance(int size, std::vector<int> board) {
+    double cartesianDistance = 0.0;
+    for (int i = 0; i < size * size; ++i) {
+        if (board[i] == 0) continue;
+        int row = i / size;
+        int col = i % size;
+        if (board[i] != i) {
+            int targetRow = (board[i] - 1) / size;
+            int targetCol = (board[i] - 1) % size;
+            cartesianDistance += std::sqrt(std::pow(row - targetRow, 2) + std::pow(col - targetCol, 2));
+        }
+    }
+    return cartesianDistance;
+}
+
+int ManhattanDistance(int size, std::vector<int> board) {
+    int manhattanDistance = 0;
+    for (int i = 0; i < size; ++i) {
+        if (board[i] == 0) continue;
+        if (board[i] != i) {
+            int targetRow = board[i] / size;
+            int targetCol = board[i] % size;
+            int row = i / size;
+            int col = i % size;
+            manhattanDistance += std::abs(row - targetRow) + std::abs(col - targetCol);
+        }
+    }
+    return manhattanDistance;
+}
---
+++ src/heuristics.h
@@ -0,0 +1,13 @@
+//
+// Created by only_u on 12/21/23.
+//
+
+#ifndef FS_PROTOTYPE_HEURISTICS_H
+#define FS_PROTOTYPE_HEURISTICS_H
+
+#include <vector>
+int HammingDistance(int size, std::vector<int> board);
+double CartesianDistance(int size, std::vector<int> board);
+int ManhattanDistance(int size, std::vector<int> board);
+
+#endif //FS_PROTOTYPE_HEURISTICS_H
