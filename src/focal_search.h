daea28d7ff81c868d66408fe218dd867e81883e4
Ashirog1 <nguyennhatquoc1012@gmail.com>
2023-12-29 15:22:31 +0700

    anytime random focal search

--- src/focal_search.h
+++ src/focal_search.h
@@ -7,6 +7,7 @@
 
 #include "game_board.h"
 #include "heuristics.h"
+#include "utils.h"
 #include <map>
 #include <queue>
 #include <utility>
@@ -86,6 +87,61 @@ public:
             if (focal.empty()) break;
             auto [f, g, board] = focal.top();
             focal.pop();
+//            std::cout << f << ' ' << g << ' ' << board << '\n';
+
+            if (visited[board] != f) continue;
+            if (board.GetHeuristic(heuristic) == 0) return static_cast<int>(f);
+            for (GameBoard &next_board: GetNeighbour(board)) {
+                if (visited.find(next_board) == visited.end() or visited[next_board] > f + 1) {
+                    visited[next_board] = f + 1;
+                    open.push({f + 1, next_board.GetHeuristic(heuristic), next_board});
+//                    std::cout << "neighbour " << next_board << '\n';
+                }
+            }
+        }
+        return static_cast<int>(-1);
+    }
+
+
+    template<class T>
+    inline int RandomWeightedFocal(std::vector<std::vector<int>> &v, T heuristic, T focal_heuristic, std::vector<double> epsilons = {1.2, 1.3}) {
+        GameBoard start(v);
+        open.push({0, start.GetHeuristic(heuristic), start});
+        visited[start] = 0;
+        std::priority_queue<Node, std::vector<Node>, CompareG> focal[epsilons.size()];
+        double best_h_score = 0;
+
+        while (not open.empty()) {
+            {
+                /// update focal
+                auto [f, g, board] = open.top();
+                double h_min = f + g;
+                best_h_score = std::max(best_h_score, h_min);
+                int focal_idx = 0;
+                double l = 1.0, r = epsilons[0];
+
+                while (not open.empty()) {
+                    auto [f_, g_, board_] = open.top();
+                    double cur_h = f_ + g_;
+                    if (cur_h > best_h_score * epsilons.back()) break;
+                    if (cur_h > best_h_score * epsilons[focal_idx]) {
+                        ++focal_idx;
+                    }
+                    open.pop();
+
+                    focal[focal_idx].push({f_, board_.GetHeuristic(focal_heuristic), board_});
+                }
+            }
+//            assert(focal.size() > 0);
+            /// find node to expand
+            double f, g; GameBoard board;
+            {
+                int focal_set = RNG::RandUniform(0, epsilons.size() - 1);
+                if (focal[focal_set].empty()) break;
+                auto [f_, g_, board_] = focal[focal_set].top();
+                f = f_; g = g_; board = board_;
+                focal[focal_set].pop();
+            }
 //            std::cout << f << ' ' << g << ' ' << board << '\n';
 
             if (visited[board] != f) continue;
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,3 +1,5 @@
 //
 // Created by only_u on 12/21/23.
 //
+
+#include "utils.h"
