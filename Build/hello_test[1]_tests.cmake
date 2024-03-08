f59b6b77a665acb0d3b6bc1333e2257ae6a2b50a
Quoc Nguyen <71763648+Ashirog1@users.noreply.github.com>
2024-03-08 15:41:55 +0700

    Delete Build directory

--- Build/hello_test[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test([=[HelloTest.BasicAssertions]=]  D:/Hiep/code/fs_prototype/Build/hello_test.exe [==[--gtest_filter=HelloTest.BasicAssertions]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[HelloTest.BasicAssertions]=]  PROPERTIES WORKING_DIRECTORY D:/Hiep/code/fs_prototype/Build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set(  hello_test_TESTS HelloTest.BasicAssertions)
